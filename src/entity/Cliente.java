6f73c1dace5265dba5cc678dad84823bd563cb6c
LoreDoe <107372991+LoreDoe@users.noreply.github.com>
2024-11-24 18:01:18 -0300

    Create Cliente.java

---
+++ src/entity/Cliente.java
@@ -0,0 +1,47 @@
+package entity;
+
+/**
+ * Classe que representa os dados de um cliente.
+ */
+public class Cliente {
+    private String nome;
+    private String sobrenome;
+    private String telefone;
+
+    // Construtor
+    public Cliente(String nome, String sobrenome, String telefone) {
+        this.nome = nome;
+        this.sobrenome = sobrenome;
+        this.telefone = telefone;
+    }
+
+    // Métodos getters e setters
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getSobrenome() {
+        return sobrenome;
+    }
+
+    public void setSobrenome(String sobrenome) {
+        this.sobrenome = sobrenome;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    @Override
+    public String toString() {
+        return nome + " " + sobrenome + " - " + telefone;
+    }
+}
